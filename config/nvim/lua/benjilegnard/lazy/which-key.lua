e6778c20c65e63543926c2fc4200673b8018b2d7
Benjamin Legrand <benjilegnard@protonmail.com>
2024-08-29 11:57:41 +0200

    fix: add nvim-web-devicons dependency to which-key ⌨️

--- config/nvim/lua/benjilegnard/lazy/which-key.lua
+++ config/nvim/lua/benjilegnard/lazy/which-key.lua
@@ -1,6 +1,9 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    },
     init = function()
         vim.o.timeout = true
         vim.o.timeoutlen = 300
