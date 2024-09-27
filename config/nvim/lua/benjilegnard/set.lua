3809a78c59c25be7e40f0e930bc5f650e5ce776a
Benjamin Legrand <benjilegnard@protonmail.com>
2024-09-27 13:35:37 +0200

    fix(neovim): set tabstop to 2

--- config/nvim/lua/benjilegnard/set.lua
+++ config/nvim/lua/benjilegnard/set.lua
@@ -2,7 +2,7 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
@@ -14,4 +14,3 @@ vim.opt.undofile = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-
