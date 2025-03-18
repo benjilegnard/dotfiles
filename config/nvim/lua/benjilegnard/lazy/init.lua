6496e21abfc95841da11988161bb4e513ad292b3
Benjamin Legrand <benjilegnard@protonmail.com>
2025-03-18 18:07:03 +0100

    fix: small changes to tmux config, restore barbar.nvim

--- config/nvim/lua/benjilegnard/lazy/init.lua
+++ config/nvim/lua/benjilegnard/lazy/init.lua
@@ -10,4 +10,7 @@ return {
 		-- optional, but required for fuzzy finder support
 		dependencies = { "nvim-telescope/telescope-fzf-native.nvim" },
 	},
+
+	-- barbar is the tabline at the top
+	"romgrk/barbar.nvim",
 }
