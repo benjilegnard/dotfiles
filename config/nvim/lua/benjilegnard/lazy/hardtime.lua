26743684895d8180e645334bb7e18a68f8f146ae
Benjamin Legrand <benjilegnard@protonmail.com>
2025-05-23 09:45:36 +0200

    feat(neovim): add hardtime.nvim

---
+++ config/nvim/lua/benjilegnard/lazy/hardtime.lua
@@ -0,0 +1,11 @@
+return {
+	"m4xshen/hardtime.nvim",
+	lazy = false,
+	dependencies = { "MunifTanjim/nui.nvim" },
+	opts = {
+		enabled = false,
+		disable_mouse = false,
+		restriction_mode = "hint",
+		disabled_keys = {},
+	},
+}
