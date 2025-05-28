71b453cc7ef835c0862d813902e554fa6c78813a
Benjamin Legrand <benjilegnard@protonmail.com>
2025-05-28 09:47:39 +0200

    fix(neovim): configure lua_lsp for nvim files

--- config/nvim/lua/benjilegnard/lazy/lspconfig.lua
+++ config/nvim/lua/benjilegnard/lazy/lspconfig.lua
@@ -45,8 +45,28 @@ return {
 						capabilities = capabilities,
 						settings = {
 							Lua = {
+								runtime = {
+									-- Tell the language server which version of Lua you're using
+									-- (most likely LuaJIT in the case of Neovim)
+									version = "LuaJIT",
+								},
 								diagnostics = {
+									-- Get the language server to recognize the `vim` global
 									globals = { "vim", "it", "describe", "before_each", "after_each" },
+								},
+								workspace = {
+									library = {
+										vim.env.VIMRUNTIME,
+										-- Depending on the usage, you might want to add additional paths here.
+										-- "${3rd}/luv/library"
+										-- "${3rd}/busted/library",
+									},
+									-- Make the server aware of Neovim runtime files
+									-- library = vim.api.nvim_get_runtime_file("", true),
+								},
+								-- Do not send telemetry data containing a randomized but unique identifier
+								telemetry = {
+									enable = false,
 								},
 							},
 						},
