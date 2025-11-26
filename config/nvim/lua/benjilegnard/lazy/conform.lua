14119846701ed396a6ff8f3a73bc635273c5f705
Benjamin Legrand <benjilegnard@protonmail.com>
2025-11-26 18:14:27 +0100

    fix(neovim): use prettier to format html with conform

--- config/nvim/lua/benjilegnard/lazy/conform.lua
+++ config/nvim/lua/benjilegnard/lazy/conform.lua
@@ -27,6 +27,11 @@ return {
 					"prettier",
 					stop_after_first = true,
 				},
+				html = {
+					"prettierd",
+					"prettier",
+					stop_after_first = true,
+				},
 			},
 			format_on_save = {
 				timeout_ms = 500,
